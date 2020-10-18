866fe1525cc514b9c383845d68f4064b02bf382f
iseki <admin@iseki.space>
2020-10-19 00:47:48 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "space.iseki"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0-M1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
